d7dfed5fddc6ff366bbad43c84938c637a51a593
The Egghead Creative <ehc@krishna.(none)>
2014-07-04 19:46:20 +0530

    Changed database from sqlite to pgsql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140703125318) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "funds", force: true do |t|
     t.text     "description"
     t.integer  "debit"
